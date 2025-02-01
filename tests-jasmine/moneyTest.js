5d0ddfc334bd27a01b5a304d0c001a02b9576e83
dian <Dianzahidd@gmail.com>
2025-01-31 21:08:29 -0500

    remove defualt tests and add moneyTest

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,23 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', ()=>{
+
+  it('convert cents into dollars', ()=>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('works with 0', ()=>{
+    expect(formatCurrency(0)).toEqual('0.00');
+   });
+
+   it('rounds up the nearest cent', ()=>{
+    expect(formatCurrency(200.5)).toEqual('2.01');
+   })
+
+
+})
+
+
+
+
+
