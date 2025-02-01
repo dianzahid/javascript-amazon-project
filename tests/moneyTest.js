aae8b7c661431ba346e8e408092f78016e08cb1a
dian <Dianzahidd@gmail.com>
2025-01-31 19:57:16 -0500

    Create tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,46 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
+console.log('test suite: formatCurrency')
+
+// *Test Case* 
+
+console.log('convert cents to dollars');
+
+if(formatCurrency(2095) == '20.95'){
+  console.log('pass')
+}
+else{
+  console.log('fail');
+}
+
+// **Edge cases**
+
+console.log('works with 0');
+
+if(formatCurrency(0) == '0.00'){
+  console.log('pass')
+}
+else{
+  console.log('fail');
+}
+
+console.log('Rounding up nearest cent');
+
+if(formatCurrency(200.5) == '2.01'){
+  console.log('pass')
+}
+else{
+  console.log('fail');
+}
+
+console.log('Rounding down nearest cent');
+
+if(formatCurrency(200.4) == '2.00'){
+  console.log('pass')
+}
+else{
+  console.log('fail');
+}
+
+
+
